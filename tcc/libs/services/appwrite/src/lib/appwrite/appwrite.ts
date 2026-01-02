f1f43976e1ea7e6d00fea59d24c3248e19b96f99
Andre <your@email.com>
2026-01-02 15:04:38 -0300

    feat: construct setup of project

--- tcc/libs/services/appwrite/src/lib/appwrite/appwrite.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-appwrite',
-  imports: [],
-  templateUrl: './appwrite.html',
-  styleUrl: './appwrite.css',
-})
-export class Appwrite {}
