9383e105b8c4162cd781d4e94af2faa687a4b22f
Andre <your@email.com>
2026-02-02 22:05:03 -0300

    feat: importação para api do ocpp-gateway

--- tcc/apps/api/src/app/app.module.ts
+++ tcc/apps/api/src/app/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { OcppSocketModule } from '@tcc/ocpp-socket'
+
 @Module({
-  imports: [],
+  imports: [ OcppSocketModule ],
   controllers: [AppController],
   providers: [AppService],
 })
