20436242434a8a25f043f68bbc1a76197320b09b
Andre <your@email.com>
2026-03-05 07:13:51 -0300

    feat(database-user): serviço para realizar as querys do conjunto dos usuários

--- tcc/lib/services/appwrite/src/lib/database/database-user.ts
+++ tcc/lib/services/appwrite/src/lib/database/database-user.ts
@@ -4,28 +4,47 @@ import { ID } from 'appwrite';
 
 import {
   User,
-  CrudServiceDB
+  CrudServiceDB,
+  Logger
 } from '@tcc/types';
 import { Database } from './database';
+
+const COLLECTION_ID = 'users_id';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DatabaseUser extends CrudServiceDB {
   private readonly database: Database = inject(Database);
+  private readonly logger: Logger = inject(Logger);
 
   constructor() {
     super();
+  }
 
+  public override async get<T>(id: string): Promise<T> {
+    const database = this.database.getDatabase();
+    const databaseId = this.database.getDatabaseId();
+
+    const response = await database.getDocument(
+      databaseId,
+      COLLECTION_ID,
+      id
+    );
+
+    return response as unknown as T;
   }
 
   public override async create<T>(data: T): Promise<void> {
     const database = this.database.getDatabase();
     const databaseId = this.database.getDatabaseId();
 
+
+    this.logger.info('Creating user', database);
+
     await database.createDocument(
       databaseId,
-      'users',
+      COLLECTION_ID,
       ID.unique(),
       data as any // O appwrite pede Record<string, any>
     );
--- tcc/lib/types/src/lib/db/crud-service-db.ts
+++ tcc/lib/types/src/lib/db/crud-service-db.ts
@@ -1,4 +1,5 @@
 export abstract class CrudServiceDB {
+    public abstract get<T>(id: string): Promise<T>;
     public abstract create<T>(data: T): Promise<void>;
     // public abstract read(): Promise<void>;
     // public abstract update(): Promise<void>;
