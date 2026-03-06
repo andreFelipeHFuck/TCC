a05d42f26b3c344e3071e3b5abc55ccf79c0e9be
Andre <your@email.com>
2026-03-05 21:18:40 -0300

    feat: introduce shared-angular library, update TypeScript path aliases, and configure initial Material theme.

---
+++ tcc/lib/services/shared/src/lib/shared-angular/shared-angular.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-shared-angular',
+  imports: [],
+  templateUrl: './shared-angular.html',
+  styleUrl: './shared-angular.css',
+})
+export class SharedAngular {}
