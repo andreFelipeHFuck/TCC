17c5fe4515379a00f0a18ea2a92bcb0c4a93cf16
Andre <your@email.com>
2026-03-08 09:18:51 -0300

    feat(button): primeira versão do botão utilizado na aplicação

--- tcc/lib/components/shared/buttons/src/lib/button/button.ts
+++ tcc/lib/components/shared/buttons/src/lib/button/button.ts
@@ -1,14 +1,18 @@
 import { Component, input } from '@angular/core';
 
+import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+
+import { ButtonColor } from '@tcc/types';
 
 @Component({
   selector: 'lib-button',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './button.html',
   styleUrl: './button.scss',
 })
 export class Button {
   title = input.required<string>();
+  color = input<ButtonColor>('primary');
 }
