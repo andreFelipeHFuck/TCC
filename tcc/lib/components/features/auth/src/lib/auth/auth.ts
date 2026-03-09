aed70512edac6667c3f4ecfd1d8b56187e71820f
Andre <your@email.com>
2026-03-09 07:43:59 -0300

    feat(title): criação do componente title na versão mobile

--- tcc/lib/components/features/auth/src/lib/auth/auth.ts
+++ tcc/lib/components/features/auth/src/lib/auth/auth.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 
+import { Title } from '@tcc/components/title';
 import { Button } from "@tcc/components/buttons"
 
 @Component({
   selector: 'lib-auth',
   imports: [
+    Title,
     Button
   ],
   templateUrl: './auth.html',
-  styleUrl: './auth.css',
+  styleUrl: './auth.scss',
 })
 export class Auth {}
