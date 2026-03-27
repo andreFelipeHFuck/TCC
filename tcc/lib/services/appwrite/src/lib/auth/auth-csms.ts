360ff80f973bf35e854b6dd43dab2e817a3861ba
Andre <your@email.com>
2026-03-27 07:01:30 -0300

    feat(auth-csms): adicionar um sistema de cache no lado do frontend para empedir chamadas repetidas

--- tcc/lib/services/appwrite/src/lib/auth/auth-csms.ts
+++ tcc/lib/services/appwrite/src/lib/auth/auth-csms.ts
@@ -4,14 +4,17 @@ import {
 } from '@angular/core';
 
 import { 
-  Logger
+  Logger,
+  UserAuth
 } from '@tcc/types';
 import { AuthBusiness } from '@tcc/models';
+
+import { CapacitorSessionService } from '@tcc/capacitor-session';
 
 import { Auth } from './auth';
 import { AuthUser } from './auth-user';
 import { AuthGrpc } from '../functions/grpc/auth/auth-grpc';
-import { CapacitorSessionService } from '@tcc/capacitor-session';
+
 
 @Injectable({
   providedIn: 'root',
@@ -28,14 +31,9 @@ export class AuthCsms {
 
   protected service = 'APPWRITE CSMS AUTH SERVICE';
 
-  async initSession() { 
-    const user = await this.authUser.getUser();
-    
-    if (user == 'NONE') {
-      this.logger.error(`[${this.service}]: Usuário não encontrado no estado local após verificação de sessão, ${user}`);
-      return;
-    }
-  
+  private async createSession(user: UserAuth): Promise<boolean> {
+    if(user == 'NONE') return false;
+
     const token = await this.auth.generateToken();
 
     const expiresAt = new Date(Date.now() + 15 * 60 * 1000);
@@ -46,14 +44,9 @@ export class AuthCsms {
       expiresAt
     );
 
-    const cachedSession = await this.capacitorSession.getSession();
-    if (cachedSession) {
-      this.logger.info(`[${this.service}]: Sessão prévia encontrada no cache: ${cachedSession}`);
-    }
-
     if (authBody === 'NONE') {
       this.logger.error(`[${this.service}]: Falha ao gerar Authentication Request válido`);
-      return;
+      return false;
     }
 
     this.logger.info(`[${this.service}] Iniciando sessão no CSMS...`);
@@ -61,10 +54,32 @@ export class AuthCsms {
 
     if (result.success && result.sessionId) {
       this.logger.info(`[${this.service}] Sessão obtida, salvando no cache persistente: ${result.sessionId}`);
-      await this.capacitorSession.setSession(result.sessionId);
+      await this.capacitorSession.setSession(result);
+    }else{
+      this.logger.error(`[${this.service}]: Falha ao obter sessão no CSMS`);
+      return false;
     }
 
     this.logger.info(`[${this.service}] Sessão iniciada com sucesso: ${JSON.stringify(result)}`);
+    return true;
+  }
+
+  async initSession(): Promise<boolean> { 
+    const user = await this.authUser.getUser();
+    
+    if (user == 'NONE') {
+      this.logger.error(`[${this.service}]: Usuário não encontrado no estado local após verificação de sessão, ${user}`);
+      return false;
+    }
+
+    const cachedSession = await this.capacitorSession.getSession();
+    if (cachedSession) {
+      this.logger.info(`[${this.service}]: Sessão prévia encontrada no cache: ${cachedSession}`);
+      return true;
+    }
+
+    const session = await this.createSession(user);
+    return session;
   }
 
 //   finishSessionProxy() { }
--- tcc/lib/services/capacitor-session/src/lib/capacitor-session.ts
+++ tcc/lib/services/capacitor-session/src/lib/capacitor-session.ts
@@ -1,51 +1,30 @@
 import { Injectable } from '@angular/core';
 import { Preferences } from '@capacitor/preferences';
 
-import { 
-  AuthenticationResponse, 
-  SessionId 
-} from '@tcc/types';
+import { AuthenticationResponse } from '@tcc/types';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CapacitorSessionService {
-  private sessionId: SessionId = 'NONE';
-
-  private setSessionId(sessionId: string): void {
-    if (
-      sessionId === 'NONE' ||
-      sessionId === '' ||
-      sessionId === null ||
-      sessionId === undefined
-    ) {
-      this.sessionId = 'NONE';
-    } else {
-      this.sessionId = sessionId;
-    }
-  }
+  private readonly sessionId: string = 'SESSION_ID';
 
   /**
    * Armazena o ID da sessão de forma persistente.
    * @param sessionId O ID da sessão retornado pelo gRPC.
    */
-  public async setSession(sessionId: string, data: AuthenticationResponse): Promise<void> {
-    this.setSessionId(sessionId);
-
-    if(this.sessionId !== 'NONE'){
-      await Preferences.set({
-        key: sessionId,
-        value: JSON.stringify(data),
-      });
-    }
+  public async setSession(data: AuthenticationResponse): Promise<void> {
+    await Preferences.set({
+      key: this.sessionId,
+      value: JSON.stringify(data),
+    });
   }
 
   /**
    * Recupera o ID da sessão armazenado.
    * @returns O ID da sessão ou null se não houver sessão.
    */
-  public async getSession(): Promise<AuthenticationResponse | 'NONE'> {
-    if(this.sessionId !== 'NONE') {
+  public async getSession(): Promise<AuthenticationResponse | null> {
       const { value } = await Preferences.get({
         key: this.sessionId,
       });
@@ -53,20 +32,17 @@ export class CapacitorSessionService {
       if(value) {
         return JSON.parse(value);
       }
-    }
-    return 'NONE';
+
+      return null;
   }
 
   /**
    * Remove a sessão armazenada (ex: logout).
    */
   public async removeSession(): Promise<void> {
-    if(this.sessionId !== 'NONE') {
-      await Preferences.remove({
-        key: this.sessionId,
-      });
-      this.setSessionId('NONE');
-    }
+    await Preferences.remove({
+      key: this.sessionId,
+    });
   }
 
   /**
@@ -74,7 +50,7 @@ export class CapacitorSessionService {
    */
   public async hasSession(): Promise<boolean> {
     const session = await this.getSession();
-    return session !== 'NONE';
+    return session !== null && session !== undefined;
   }
 
   /**
