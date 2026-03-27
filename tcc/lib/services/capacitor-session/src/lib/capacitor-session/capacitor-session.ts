a25cfc5c7033ff8ff89a2859e3ed6219c758def7
Andre <your@email.com>
2026-03-27 07:01:30 -0300

    feat(auth-csms): adicionar um sistema de cache no lado do frontend para empedir chamadas repetidas

--- tcc/lib/services/capacitor-session/src/lib/capacitor-session/capacitor-session.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-capacitor-session',
-  imports: [],
-  templateUrl: './capacitor-session.html',
-  styleUrl: './capacitor-session.css',
-})
-export class CapacitorSession {}
