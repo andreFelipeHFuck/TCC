2307b05a9a75fc6705e0c66e8c54e155f060902a
Andre <your@email.com>
2026-03-29 11:15:11 -0300

    feat: serviços agnosticos para autentificação, permitindo que se seja feito alterações do backend se for necessário

--- tcc/lib/services/appwrite/src/lib/appwrite-provide.ts
+++ tcc/lib/services/appwrite/src/lib/appwrite-provide.ts
@@ -1,9 +1,17 @@
 import { Provider } from "@angular/core";
 
-import { AppwriteConfig } from "@tcc/types";
+import { 
+  AppwriteConfig, 
+  AUTH_DRIVER, 
+  DATABASE_DRIVER, 
+  AUTH_SERVICE 
+} from "@tcc/types";
+import { AuthSystem } from "@tcc/core";
 
 import { APPWRITE_CONFIG, APPWRITE_DATABASE_ID } from './appwrite-connections/appwrite-token';
 import { Appwrite } from "./appwrite";
+import { Auth } from "./auth/auth";
+import { DatabaseUser } from "./database/database-user";
 
 export function appwriteProvide(
   config: AppwriteConfig,
@@ -17,6 +25,20 @@ export function appwriteProvide(
     {
       provide: APPWRITE_DATABASE_ID,
       useValue: config.databaseId,
+    },
+    // Registrar os drivers agnósticos usando as implementações do Appwrite
+    {
+      provide: AUTH_DRIVER,
+      useClass: Auth,
+    },
+    {
+      provide: DATABASE_DRIVER,
+      useClass: DatabaseUser,
+    },
+    // O sistema central agora usa o AuthSystem do Core
+    {
+      provide: AUTH_SERVICE, // Agora o Token substitui a interface
+      useClass: AuthSystem,
     }
   ];
 }
